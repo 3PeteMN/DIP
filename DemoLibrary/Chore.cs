b932dcb3d3855ff055d04d0dcc018c5224e6c1c2
3PeteMN <3pmeyer@comcast.net>
2019-11-22 21:15:46 -0600

    Add project files.

---
+++ DemoLibrary/Chore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Chore : IChore
+    {
+        public string ChoreName { get; set; }
+        public IPerson Owner { get; set; }
+        public double HoursWorked { get; private set; }
+        public bool IsComplete { get; private set; }
+
+        private ILogger _logger;
+        private IMessageSender _messageSender;
+
+        public Chore(ILogger log, IMessageSender messageSender)
+        {
+            _logger = log;
+            _messageSender = messageSender;
+        }
+
+        public void PerformedWork(double hours)
+        {
+            HoursWorked += hours;
+            _logger.Log($"Performed work on { ChoreName }");
+        }
+
+        public void CompleteChore()
+        {
+            IsComplete = true;
+
+            _logger.Log($"Completed { ChoreName }");
+
+            _messageSender.SendMessage(Owner, $"The chore { ChoreName } is complete.");
+        }
+    }
+}
---
+++ DemoLibrary/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            //Console.WriteLine($"Write to Console: { message }");
+            Console.WriteLine($"Write to Console: { message } using new logger");
+        }
+    }
+}
